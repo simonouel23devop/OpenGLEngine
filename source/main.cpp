2b7893b45185b2bcb68023a94d1890ff5187a0c3
Simmus <simonouel12@gmail.com>
2026-09-06 23:46:01 -0400

    Update main.cpp
    Update buffer swap

--- source/main.cpp
+++ source/main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include <GL/glew.h>
 #if defined(OPENGL_ENGINE_USE_GLFW) && OPENGL_ENGINE_USE_GLFW
 #if __has_include(<GLFW/glfw3.h>)
 #include <GLFW/glfw3.h>
@@ -17,9 +17,19 @@
 
 int main()
 {
+
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    
 #if !GLFW_AVAILABLE
     std::cerr << "GLFW is not configured. Add its include directory to the project settings.\n";
     return -1;
+
+
+
+
 #else
     if (!glfwInit())
     {
@@ -38,12 +48,21 @@ int main()
     glfwSetWindowPos(window,500, 150);
     glfwMakeContextCurrent(window);
 
+	if (glewInit() != GLEW_OK)
+    {
+		glfwTerminate();
+		return -1;
+    }
+
+
+
 
 
 while (!glfwWindowShouldClose(window))
     {
-        // Rendering code would go here
-
+        
+	    glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
